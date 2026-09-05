48ec854425d7a0736ce8488a6c38c21a7d9b2cca
Judah Thurman <jnt979@utexas.edu>
2026-09-05 13:57:14 -0500

    created movie and rating tables in schema

--- sql/schema_setup.sql
+++ sql/schema_setup.sql
@@ -3,3 +3,15 @@ CREATE TABLE movies (
     title     VARCHAR(255) NOT NULL,
     genres     VARCHAR(255) NOT NULL
 );
+
+SELECT * FROM movies LIMIT 10;
+
+CREATE TABLE ratings (
+    user_id  INT NOT NULL,
+    movie_id INT NOT NULL REFERENCES movies(movie_id),
+    rating   DECIMAL(2, 1) NOT NULL,
+    timestamp BIGINT NOT NULL,
+    PRIMARY KEY (user_id, movie_id)
+);
+
+SELECT * FROM ratings LIMIT 10;

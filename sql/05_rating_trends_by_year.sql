18456125fc7c50d0f3cc9c5e6113082d0a88f452
Judah Thurman <jnt979@utexas.edu>
2026-09-07 12:37:00 -0500

    completed rated trends and starting genre popularity

---
+++ sql/05_rating_trends_by_year.sql
@@ -0,0 +1,9 @@
+SELECT
+    m.release_year,
+    ROUND(AVG(r.rating), 2) AS average_rating,
+    COUNT(r.rating) AS rating_count
+FROM ratings r
+JOIN movies m ON r.movie_id = m.movie_id
+WHERE m.release_year IS NOT NULL
+GROUP BY m.release_year
+ORDER BY m.release_year;
